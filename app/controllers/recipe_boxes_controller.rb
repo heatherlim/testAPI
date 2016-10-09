84b857e54cd4eaa1faf4cf72b2d997dc00a56498
github username <hhl262@nyu.edu>
2016-10-09 18:05:24 -0400

    Added Routes

--- app/controllers/recipe_boxes_controller.rb
+++ app/controllers/recipe_boxes_controller.rb
@@ -1,2 +1,8 @@
 class RecipeBoxesController < ApplicationController
+  
+  def index
+    @recipe_boxes = RecipeBox.all
+    render :json => @recipe_boxes
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/api/users', to:'users#index'
+  get '/api/recipe_boxes', to:'recipe_boxes#index'
+  get '/api/recipes', to:'recipes#index'
+  get '/api/ingredients', to:'ingredients#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
