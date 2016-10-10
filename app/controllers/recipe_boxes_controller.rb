f61ec580d9ba57d70e5a668b8bfc1b4f402aeeba
github username <hhl262@nyu.edu>
2016-10-09 20:20:29 -0400

    added active model serializer

--- app/controllers/recipe_boxes_controller.rb
+++ app/controllers/recipe_boxes_controller.rb
@@ -1,8 +1,17 @@
 class RecipeBoxesController < ApplicationController
   
   def index
-    @recipe_boxes = RecipeBox.all
-    render :json => @recipe_boxes
+    if params[:user_id].blank?
+      @recipe_boxes = RecipeBox.all
+      render :json => @recipe_boxes
+    else
+      @recipe_box = RecipeBox.where(user_id: params[:user_id])
+      render :json => @recipe_box
+      
+    end
   end
+  
+  
+  
   
 end
---
+++ app/serializers/recipe_box_serializer.rb
@@ -0,0 +1,5 @@
+class RecipeBoxSerializer < ActiveModel::Serializer
+  has_one :user
+  has_many :recipes
+  attributes :id, :name
+end
