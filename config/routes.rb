7a3ef2ba287f517370dad72a24df4bcd3dfeb8f1
github username <hhl262@nyu.edu>
2016-10-10 02:56:27 -0400

    filled up doc page

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   
   scope '/api' do
     get '/users', to:'users#index'
+    get '/users/:id', to: 'users#show', as: 'user'
     
     get '/recipe_boxes', to: 'recipe_boxes#index'
     
     get '/recipes', to:'recipes#index'
-    get '/recipes/:id', to: 'recipes#show'
+    get '/recipes/:id', to: 'recipes#show', as: 'recipe'
     
     get '/ingredients', to:'ingredients#index'
   end
